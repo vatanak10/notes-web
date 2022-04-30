fffbddac78bb880e3d5e5683c99e9cfbd2db48e6
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-04-30 20:02:05 +0700

    :art: setting up styling components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar /> {/* Can also use <Sidebar></Sidebar> */}
       <div className="content">
         <h1>App Component</h1>
       </div>
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1>This is Sidebar</h1>
+        </div>
+    );
+}
+ 
+export default Sidebar;
