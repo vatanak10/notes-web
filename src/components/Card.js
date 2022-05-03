5fee3827fda525b4f4728ab27cf68cadc2d9666f
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-05-03 21:59:55 +0700

    :iphone: card display

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+const Card = ({bgColor}) => {
+    return (
+    <div className={`${bgColor} w-80 h-80 rounded-3xl p-6 mr-6 mb-6`}>
+        This is Card
+    </div>
+    );
+}
+
+export default Card;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,21 @@
+import Card from './Card';
+
 const Dashboard = () => {
     return (
-        <div className="content px-20 pt-10">
+        <div className="h-screen px-20 pt-10">
             <h1 className="text-5xl font-bold my-5">
                 Notes
             </h1>
-            <div className="bg-[#F8F8F8] text-[#9C9C9C] max-w-md rounded-xl p-2 pl-4 my-6">
-                Search...
+            <input type="text" placeholder='Search...' className="bg-[#F8F8F8] text-[#9C9C9C] max-w-md rounded-xl p-2 pl-4 my-6"/>
+            <div className='h-4/5 overflow-x-auto flex flex-col flex-wrap'>
+                <Card bgColor="bg-card-1"/>
+                <Card bgColor="bg-card-2"/>
+                <Card bgColor="bg-card-3"/>
+                <Card bgColor="bg-card-4"/>
+                <Card bgColor="bg-card-1"/>
+                <Card bgColor="bg-card-2"/>
+                <Card bgColor="bg-card-3"/>
+                <Card bgColor="bg-card-4"/>
             </div>
         </div>
     );
